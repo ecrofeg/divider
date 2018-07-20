cfc4617e72789ac1945a2545d58f8f6d706f1f9b
pnaumov <ecrofeg1@gmail.com>
2018-07-21 01:21:14 +0400

    text/plain

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/', (req, res) => {
-	res.json(req.query.a / req.query.b);
+	res.set('Content-Type', 'text/plain');
+	res.send((req.query.a / req.query.b) + '');
 });
 
 module.exports = app;
